a54190b85ced83a63fb395d0a58e52648bbf4e80
Tim Littlemore <tim@tjl2.com>
2023-04-01 00:29:16 +0100

    First stab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [backgroundImage, setBackgroundImage] = useState("");
+
+  const handleChangeBackground = (faction) => {
+    faction === "astartes" ? 
+      setBackgroundImage("url('../40K-Terminators.jpg')") :
+      setBackgroundImage("url('../40K-Tyranids.jpg')");
+  };
+
+  const containerStyles = {
+    backgroundImage: backgroundImage
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <main className="App-main" style={containerStyles}>
+      <div className="faction-buttons">
+          <FactionBtn faction="astartes" onClick={() => handleChangeBackground("astartes")} />
+          <FactionBtn faction="tyranids" onClick={() => handleChangeBackground("tyranids")} />
+        </div>
+        <Me />
+        <a href="https://timlittlemore.s3-eu-west-1.amazonaws.com/Tim+Littlemore+CV.pdf" target="_blank" rel="noreferrer">
+          <button>Download CV</button>
         </a>
-      </header>
+      </main>
     </div>
   );
+}
+
+function Me() {
+  return (
+    <div className="Me">
+      <h1>Tim Littlemore</h1>
+      <h2>Senior Software Engineer | Infrastructure Engineer</h2>
+      <p>All the details about how much I love 40K and how I'd love to work for Games Workshop.</p>
+      <h2>About the site</h2>
+      <ul>
+        <li>React</li>
+        <li>Deployed via AWS CDK</li>
+        <li>Hosted on S3</li>
+        <li>CloudFront CDN</li>
+        <li>Link to repo</li>
+      </ul>
+    </div>
+  )
+}
+
+function FactionBtn({faction, onClick }) {
+  const factionText = faction === "astartes" ? "Loyal to the Emperor" : "Xenos scum";
+  
+  return (
+    <>
+    <button onClick={onClick}>
+      {factionText}
+    </button>
+    </>
+  )
 }
 
 export default App;
